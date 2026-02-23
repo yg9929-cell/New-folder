2b0fb167e1a1df0e7a072d4a95fb597d52aa8160
YOSHITHA GATTATH <yg9929@srmist.edu.in>
2026-02-23 09:23:57 +0530

    third

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,53 @@
-public class OOPSBannerApp{
-	public static void main(String[] args){
-	System.out.println("OOPS");
-	}
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("  ",
+            "  ***  ",
+            "  ***  ",
+            " ***** ",
+            " ***** "
+        ));
+
+        System.out.println(String.join("  ",
+            " *   * ",
+            " *   * ",
+            " *    *",
+            " *     "
+        ));
+
+        System.out.println(String.join("  ",
+            " *   * ",
+            " *   * ",
+            " *    *",
+            " *     "
+        ));
+
+        System.out.println(String.join("  ",
+            " *   * ",
+            " *   * ",
+            " ***** ",
+            " ***** "
+        ));
+
+        System.out.println(String.join("  ",
+            " *   * ",
+            " *   * ",
+            " *     ",
+            "     * "
+        ));
+
+        System.out.println(String.join("  ",
+            " *   * ",
+            " *   * ",
+            " *     ",
+            "     * "
+        ));
+
+        System.out.println(String.join("  ",
+            "  ***  ",
+            "  ***  ",
+            " *     ",
+            " ***** "
+        ));
+    }
 }
